da3766af2f1c6d953d6bc68cfdf60db121ec28d5
Karthik S <37337298+ksathiya88@users.noreply.github.com>
2019-06-16 12:55:35 +0530

    Adding basic authentication

--- server.js
+++ server.js
@@ -1,117 +1,144 @@
 // set up ========================
-var express = require('express');
-var app = express();                               // create our app w/ express
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const basicAuth = require('_helpers/basic-auth');
+const errorHandler = require('_helpers/error-handler');
 
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 
 const employees = [
-  {
-    "key": 1,
-    "name": "ABC",
-    "date_of_birth": "10/10/2010",
-    "position_held": "Manager"
-  },
-  {
-    "key": 2,
-    "name": "John",
-    "date_of_birth": "10/10/2010",
-    "position_held": "Manager"
-  },
-  {
-    "key": 3,
-    "name": "Michael",
-    "date_of_birth": "10/10/2010",
-    "position_held": "Manager"
-  },
-  {
-    "key": 4,
-    "name": "Michael",
-    "date_of_birth": "10/10/2010",
-    "position_held": "Manager"
-  },
-  {
-    "key": 5,
-    "name": "Michael",
-    "date_of_birth": "10/10/2010",
-    "position_held": "Manager"
-  },
-  {
-    "key": 6,
-    "name": "Michael",
-    "date_of_birth": "10/10/2010",
-    "position_held": "Manager"
-  }
+    {
+        "key": 1,
+        "name": "ABC",
+        "date_of_birth": "10/10/2010",
+        "position_held": "Manager"
+    },
+    {
+        "key": 2,
+        "name": "John",
+        "date_of_birth": "10/10/2010",
+        "position_held": "Manager"
+    },
+    {
+        "key": 3,
+        "name": "Michael",
+        "date_of_birth": "10/10/2010",
+        "position_held": "Manager"
+    },
+    {
+        "key": 4,
+        "name": "Michael",
+        "date_of_birth": "10/10/2010",
+        "position_held": "Manager"
+    },
+    {
+        "key": 5,
+        "name": "Michael",
+        "date_of_birth": "10/10/2010",
+        "position_held": "Manager"
+    },
+    {
+        "key": 6,
+        "name": "Michael",
+        "date_of_birth": "10/10/2010",
+        "position_held": "Manager"
+    }
 ];
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-//  app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-//app.use(methodOverride());
+// use basic HTTP auth to secure the api
+app.use(basicAuth);
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+//
+//
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,X-Auth-Token");
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//     next();
+// });
+
+// app.post('users/authenticate', function (req, res) {
+//
+//     console.log("authentication",req.body);
+//     //console.log("employees", employees);
+//     if (req.body.username === "test" && req.body.password === "abc") {
+//         res.send({'user':'test','password': 'abc'});
+//     } else {
+//         res.status(401).send('Not a User');
+//     }
+// });
 
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+// app.use(basicAuth({
+//     users: {'test': 'abc'}
+// }));
 
 app.get('/getEmployees', function (req, res) {
-  console.log("employees", employees);
-  res.send(employees);
+    console.log("employees", employees);
+    res.send(employees);
 });
+
 
 app.get('/getEmployee', function (req, res) {
-  console.log("req", req);
-  let key = req.query.param;
-  let employee = employees.find((emp) => {
-    return emp.key == key;
-  });
-  res.send(employee);
+    console.log("req", req);
+    let key = req.query.param;
+    let employee = employees.find((emp) => {
+        return emp.key == key;
+    });
+    res.send(employee);
 });
 
-app.put('/authenticate', function (req, res) {
-  console.log("authenticate request", req.body);
-  let cred = req.body;
-  console.log("credentials", cred, cred.username, cred.password);
-  if (cred.username == "abc" && cred.password == "pass") {
-    res.send({});
-  } else {
-    res.status(401).send('Not a User');
-  }
-});
+// app.put('/authenticate', function (req, res) {
+//     console.log("authenticate request", req.body);
+//     let cred = req.body;
+//     console.log("credentials", cred, cred.username, cred.password);
+//     if (cred.username == "abc" && cred.password == "pass") {
+//         res.send({});
+//     } else {
+//         res.status(401).send('Not a User');
+//     }
+// });
 
 app.delete('/deleteEmployee', function (req, res) {
-  console.log("delete employees", employees);
-  var employee = employees.find((employee) => {
-    return employee.key == req.query.key;
-  });
-  employees.splice(employees.indexOf(employee), 1);
-  res.send(employees);
-  //console.log("employee",employee);
-  //console.log("id value",req.query);
+    console.log("delete employees", employees);
+    var employee = employees.find((employee) => {
+        return employee.key == req.query.key;
+    });
+    employees.splice(employees.indexOf(employee), 1);
+    res.send(employees);
+    //console.log("employee",employee);
+    //console.log("id value",req.query);
 })
 
 app.put('/addEmployee', function (req, res) {
-  console.log("add employees", req.body);
+    console.log("add employees", req.body);
 
-  let employee = req.body;
-  console.log("employees", employees);
-  employee["key"] = employees[employees.length - 1]["key"] + 1;
-  employees.push(employee);
+    let employee = req.body;
+    console.log("employees", employees);
+    employee["key"] = employees[employees.length - 1]["key"] + 1;
+    employees.push(employee);
 
-  console.log(employee, employee.name);
-  res.send(employees);
+    console.log(employee, employee.name);
+    res.send(employees);
 
 });
 
 
-app.get('*', function (req, res) {
-  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
+// app.get('*', function (req, res) {
+//     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
---
+++ users/user.service.js
@@ -0,0 +1,25 @@
+﻿// users hardcoded for simplicity, store in a db for production applications
+const users = [{ id: 1, username: 'abc', password: 'pass', firstName: 'abc', lastName: 'pass' }];
+
+module.exports = {
+    authenticate,
+    getAll
+};
+
+async function authenticate({ username, password }) {
+
+    console.log("username---", username);
+    console.log("password----", password);
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+}
+
+async function getAll() {
+    return users.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
